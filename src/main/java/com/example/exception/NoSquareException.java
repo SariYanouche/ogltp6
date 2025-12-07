be20f0fd84c611e4f046ba35d54fae7d3b705db4
hako0o00 <la_amini@esi.dz>
2025-12-07 15:11:04 +0100

    version 1

---
+++ src/main/java/com/example/exception/NoSquareException.java
@@ -0,0 +1,18 @@
+package com.example.exception;
+
+public class NoSquareException extends Exception {
+
+    public NoSquareException() {
+        super();
+        // TODO Auto-generated constructor stub
+
+    }
+
+    public NoSquareException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+}
+
+
